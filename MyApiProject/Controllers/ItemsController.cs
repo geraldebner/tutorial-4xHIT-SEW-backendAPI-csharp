13b9def21d68c828c71a5becb0a5673ed9041896
Gerald Ebner <gebner@lr.htlweiz.at>
2025-11-15 14:50:52 +0100

    get put

--- MyApiProject/Controllers/ItemsController.cs
+++ MyApiProject/Controllers/ItemsController.cs
@@ -1,19 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
 
 namespace MyApiProject.Controllers;
+
 [ApiController]
-[Route("api/[controller]")]
 public class ItemsController : ControllerBase
 {
-    [HttpGet]
-    public IActionResult GetItems()
+    // Variable to store text (simulating global variable)
+    private static string storedText = "";
+
+    // GET /
+    [HttpGet("/")]
+    public IActionResult ReadHello()
     {
-        return Ok(new string[] { "Item1", "Item2", "Item3" });
+        return Ok(new { message = "Hello API!" });
     }
 
-    [HttpGet("{id}")]
-    public IActionResult GetItem(int id)
+    // GET /items/{item_id}
+    [HttpGet("/items/{item_id}")]
+    public IActionResult ReadItem(int item_id, [FromQuery] string? q = null)
     {
-        return Ok(new { Id = id, Name = $"Item{id}" });
+        return Ok(new { item_id, query = q });
+    }
+
+    // PUT /text
+    [HttpPut("/text")]
+    public IActionResult StoreText(string text)
+    {
+        storedText = text;
+        return Ok("ok");
+    }
+
+    // GET /text
+    [HttpGet("/text")]
+    public IActionResult GetText()
+    {
+        return Ok(storedText);
     }
 }
